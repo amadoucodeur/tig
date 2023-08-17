6d35e980f6c5bffedd1becc0da74a1f652ddecd6
amadoucodeur <amadoucodeur@gmail.com>
2023-08-17 18:07:54 +0000

    cool paramiter

--- tig.py
+++ tig.py
@@ -2,8 +2,7 @@ import subprocess
 import sys
 
 
+
 subprocess.check_output('git add .',shell=True, text=True)
 subprocess.check_output(f'git commit -m "{sys.argv[1]}"',shell=True, text=True)
-# subprocess.check_output(commandes,shell=True, text=True)
-
-print(sys.argv)
+subprocess.check_output('git push',shell=True, text=True)
